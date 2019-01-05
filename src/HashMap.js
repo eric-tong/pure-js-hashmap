688d9ca413b0e962d1fcd3154d83b8d8f9ca0f01
eric-tong <eric.yintong@gmail.com>
2019-01-05 09:56:50 +0000

    Rename file

---
+++ src/HashMap.js
@@ -0,0 +1,52 @@
+import MapNode from './MapNode';
+import hashCode from './hashCode';
+
+export default class HashMap {
+    constructor() {
+        this._capacity = 2 ** 4;
+        this._buckets = Array.from({ length: this._capacity }, (v, k) => new MapNode());
+    }
+
+    set(key, value) {
+        let node = this.getBucketNode(key);
+        while (node.next) {
+            if (node.next.key === key) removeNextNode(node);
+            node = node.next;
+        }
+        node.next = new MapNode(key, value);
+    }
+
+    get(key) {
+        let node = this.getBucketNode(key);
+        while (node.next) {
+            if (node.next.key === key) return node.next.value;
+        }
+        return undefined;
+    }
+
+    delete(key) {
+        let node = this.getBucketNode(key);
+        while (node.next) {
+            if (node.next.key === key) {
+                removeNextNode(node);
+                break;
+            }
+        }
+    }
+
+    clear() {
+        this._buckets.forEach((_, index, array) => array[index].next = null);
+    }
+
+    getBucketNode(key) {
+        return this._buckets(this.getBucket(key));
+    }
+
+    getBucket(key) {
+        return hashCode(key) % this._capacity;
+    }
+
+    removeNextNode(node) {
+        node.next = node.next.next;
+    }
+}
